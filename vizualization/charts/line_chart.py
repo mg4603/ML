17b025b9b16bde848fdefc5f5a0670361fb095c1
Michael George <michael.g.george@gmail.com>
2022-10-13 14:59:56 +0530

    Create fn for line_chart in visualization

---
+++ vizualization/charts/line_chart.py
@@ -0,0 +1,26 @@
+from matplotlib import pyplot as plt
+
+years = [1950, 1960, 1970, 1980, 1990, 2000, 2010]
+gdp = [300.2, 543.3, 1075.9, 2862.5, 5979.6, 10289.7, 14958.3]
+
+
+def line_chart(xvalues, yvalues, color='green', marker='o', 
+               linestyle='solid', title='', xlabel='', ylabel=''):
+    plt.plot(
+        xvalues, yvalues, 
+        color=color, 
+        marker=marker, 
+        linestyles=linestyle
+    )
+
+    if title:
+        plt.title(title)
+
+    if ylabel:
+        plt.ylabel(ylabel)
+
+    if xlabel:
+        plt.xlabel(xlabel)
+
+    plt.show()
+
