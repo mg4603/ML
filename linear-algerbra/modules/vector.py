f9a35b4d9da90e9215b49e97e4e90f1ec046b237
Michael George <michael.g.george@gmail.com>
2022-10-13 21:30:20 +0530

    Add helper fn isNum to Vector in linear-algebra

--- linear-algerbra/modules/vector.py
+++ linear-algerbra/modules/vector.py
@@ -2,6 +2,13 @@ class Vector:
     def __init__(self, vals):
         self.vals = [val for val in vals]
 
+    def isNum(num):
+        try:
+            float(num)
+            False
+        except ValueError:
+            return False
+            
     def append(self, new_val):
         if new_val:
             self.vals.append(new_val)
