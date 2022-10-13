4c137dba979a5f33a519f1676935b4a9cc1086eb
Michael George <michael.g.george@gmail.com>
2022-10-13 20:52:06 +0530

    Add append method to Vector class in linear-algebra

--- linear-algerbra/modules/vector.py
+++ linear-algerbra/modules/vector.py
@@ -1,3 +1,9 @@
 class Vector:
     def __init__(self, vals):
         self.vals = [val for val in vals]
+
+    def append(self, new_val):
+        if new_val:
+            self.vals.append(new_val)
+    
+    
