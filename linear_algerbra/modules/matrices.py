bf32e8caf82c31939b69278386d474733d458259
Michael George <michael.g.george@gmail.com>
2022-10-14 19:23:47 +0530

    Modify Matrix constructor
    Fixed error in arguments access in Matrix constructor.

--- linear_algerbra/modules/matrices.py
+++ linear_algerbra/modules/matrices.py
@@ -1,10 +1,10 @@
 class Matrix:
     def __init__(self, *matrix):
         if matrix:
-            self.num_rows = len(matrix)
-            self.num_cols = len(matrix[0]) if matrix[0] else 0
-            data_type = type(matrix[self.num_rows-1][self.num_cols-1])
-            self.matrix = [aij for ai in matrix for aij in ai if type(aij) is data_type]
+            self.num_rows = len(matrix[0])
+            self.num_cols = len(matrix[0][0]) if matrix[0] else 0
+            data_type = type(matrix[0][self.num_rows-1][self.num_cols-1])
+            self.matrix = [[aij  for aij in ai if type(aij) is data_type] for ai in matrix[0] ]
         else:
             self.matrix = []
 
