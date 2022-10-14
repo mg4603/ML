9a6f9a2c230659a33be572155c335e757d3ec106
Michael George <michael.g.george@gmail.com>
2022-10-14 20:43:46 +0530

    Test Matrix method get_row

--- linear_algerbra/tests/test_matrix.py
+++ linear_algerbra/tests/test_matrix.py
@@ -1,4 +1,5 @@
 from modules.matrices import Matrix
+from pytest import raises
 
 def test_Matrix():
     mat = Matrix([[1, 2, 3, 4], [2, 4, 6, 8]])
@@ -12,3 +13,14 @@ def test_shape():
     assert mat.shape() == (2, 4)
     mat = Matrix()
     assert mat.shape() == (0, 0)
+
+def test_get_row():
+    mat = Matrix([[1, 2, 3, 4], [2, 4, 6, 8]])
+    assert mat.get_row(1) == [1, 2, 3, 4]
+    assert mat.get_row(2) == [2, 4, 6, 8]
+
+    with raises(IndexError):
+        mat.get_row(0)
+        
+    with raises(IndexError):
+        mat.get_row(5)
