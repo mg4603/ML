1eb849e070c96920a79e7bdca95c2377c4a7a4aa
Michael George <michael.g.george@gmail.com>
2022-10-14 17:53:20 +0530

    Test Matrix class

---
+++ linear_algerbra/tests/test_matrix.py
@@ -0,0 +1,8 @@
+from modules.matrices import Matrix
+
+def test_Matrix():
+    mat = Matrix([[1, 2, 3, 4], [2, 4, 6, 8]])
+    mat2 = Matrix()
+    assert mat.matrix[0][0] == 1
+    assert len(mat2.matrix) == 0
+    assert type(mat2.matrix) is type([])
