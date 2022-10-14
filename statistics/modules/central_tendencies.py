9e93a04f8b8f4fb90cab9283f72bd3872959e305
Michael George <michael.g.george@gmail.com>
2022-10-14 23:39:39 +0530

    Add fn median to central_tendencies

--- statistics/modules/central_tendencies.py
+++ statistics/modules/central_tendencies.py
@@ -14,3 +14,20 @@ def mean(x):
             return sum(x)/ len(x)
     except ValueError:
         raise Exception("Requires a homogenous collection of numbers") 
+
+def median(x):
+    if not is_collection(x):
+        raise Exception("Mean doesn't make sense w.r.t input")
+
+    try:
+        if all(isinstance(float(val), float) for val in x):
+            n = len(x)
+            x = sorted(x)
+            midpoint = n//2
+            if n % 2 == 1:
+                return x[midpoint]
+            else:
+                return (x[midpoint-1] + x[midpoint])/2
+    except ValueError:
+        raise Exception("Requires a homogenous collection of numbers") 
+
