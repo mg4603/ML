0a68f20b08f4b320ba331ee02d9e46c3f229ba42
Michael George <michael.g.george@gmail.com>
2022-10-15 00:25:36 +0530

    Test fn quantile in test_central_tendencies

--- statistics/tests/test_central_tendencies.py
+++ statistics/tests/test_central_tendencies.py
@@ -1,4 +1,4 @@
-from modules.central_tendencies import is_collection, mean, median
+from modules.central_tendencies import is_collection, mean, median, quantile
 from pytest import raises
 
 def test_is_collection():
@@ -26,4 +26,11 @@ def test_median():
     with raises(Exception):
         median('a')
 
-
+def test_quantile():
+    assert quantile([1, 2, 3, 4, 5], 0.1) == 1
+    assert quantile([1, 2, 3, 4, 5], 0.2) == 2
+    with raises(Exception):
+        quantile([1, 2, 3, 4, 'a'], 0.1)
+    
+    with raises(Exception):
+        quantile('a', 0.1)
