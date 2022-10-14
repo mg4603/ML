b7e8a61925609a00ae355895b431973a17c70e94
Michael George <michael.g.george@gmail.com>
2022-10-14 23:34:16 +0530

    Add fn mean to central_tendencies

--- statistics/modules/central_tendencies.py
+++ statistics/modules/central_tendencies.py
@@ -5,3 +5,12 @@ def is_collection(collection):
     except TypeError:
         return False
         
+def mean(x):
+    if not is_collection(x):
+        raise Exception("Mean doesn't make sense w.r.t input")
+
+    try:
+        if all(isinstance(float(val), float) for val in x):
+            return sum(x)/ len(x)
+    except ValueError:
+        raise Exception("Requires a homogenous collection of numbers") 
