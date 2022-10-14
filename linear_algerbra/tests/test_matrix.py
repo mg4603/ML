5343dd7e84b0e53411c5ff7ace736240bd7ee87c
Michael George <michael.g.george@gmail.com>
2022-10-14 19:25:55 +0530

    Test Matrix method shape

--- linear_algerbra/tests/test_matrix.py
+++ linear_algerbra/tests/test_matrix.py
@@ -6,3 +6,7 @@ def test_Matrix():
     assert mat.matrix[0][0] == 1
     assert len(mat2.matrix) == 0
     assert type(mat2.matrix) is type([])
+
+def test_shape():
+    mat = Matrix([[1, 2, 3, 4], [2, 4, 6, 8]])
+    assert mat.shape() == (2, 4)
