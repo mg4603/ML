c1c283d71f7d5dbc3379ac92b2685ada70aa24ce
Michael George <michael.g.george@gmail.com>
2022-10-14 19:15:40 +0530

    Rename attributes of Matrix class to be representative

--- linear_algerbra/modules/matrices.py
+++ linear_algerbra/modules/matrices.py
@@ -1,9 +1,10 @@
 class Matrix:
     def __init__(self, *matrix):
         if matrix:
-            m = len(matrix)
-            n = len(matrix[0]) if matrix[0] else 0
-            data_type = type(matrix[m-1][n-1])
+            self.num_rows = len(matrix)
+            self.num_cols = len(matrix[0]) if matrix[0] else 0
+            data_type = type(matrix[self.num_rows-1][self.num_cols-1])
             self.matrix = [aij for ai in matrix for aij in ai if type(aij) is data_type]
         else:
             self.matrix = []
+
