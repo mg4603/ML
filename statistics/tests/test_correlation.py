4c6842a57c00bb93ea643a0d789bbc818fd0d9f7
Michael George <michael.g.george@gmail.com>
2022-10-15 15:56:37 +0530

    Test fn correlation in correlation, statistics

--- statistics/tests/test_correlation.py
+++ statistics/tests/test_correlation.py
@@ -1,5 +1,6 @@
 from modules.correlation import *
 from pytest import raises
+from math import sqrt 
 
 def test_dot():
     with raises(Exception):
@@ -18,3 +19,14 @@ def test_covariance():
         covariance([1, 2], [3, 4, 5])
     
     assert covariance([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]) == 2.5
+
+def test_correlation():
+    with raises(Exception):
+        correlation(1, 2)
+    with raises(Exception):
+        correlation(1, [1, 2])
+    with raises(Exception):
+        correlation([1, 2], [3, 4, 5])
+    
+    assert correlation([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]) == (2.5 / sqrt(2.5) / sqrt(2.5))
+    assert correlation([1, 2, 3, 4, 5], [0, 0, 0, 0, 0]) == 0
