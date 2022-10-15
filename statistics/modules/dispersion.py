b532a43fd8f140c1d895806c83cb2131bb315c08
Michael George <michael.g.george@gmail.com>
2022-10-15 11:41:38 +0530

    Add fn de_mean to dispersion, statistics

--- statistics/modules/dispersion.py
+++ statistics/modules/dispersion.py
@@ -1,3 +1,5 @@
+from modules.central_tendencies import mean
+
 def is_iterable(x):
     try:
         min(x)
@@ -10,3 +12,7 @@ def data_range(x):
         return max(x) - min(x)
     else:
         raise Exception('Range can\'t be calculated on given input')
+
+def de_mean(x):
+    x_bar = mean(x)
+    return [x_i - x_bar for x_i in x]
