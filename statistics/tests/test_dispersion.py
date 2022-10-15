b3475deef3927049cf5628bf7f627a3a60290eac
Michael George <michael.g.george@gmail.com>
2022-10-15 11:35:36 +0530

    Test fn data_range in test_dispersion, statistics

--- statistics/tests/test_dispersion.py
+++ statistics/tests/test_dispersion.py
@@ -6,3 +6,8 @@ def test_is_iterable():
     assert is_iterable([1, 2, 3, 4])
     assert is_iterable('asdf')
     assert not is_iterable(1)
+
+def test_data_range():
+    assert data_range([1, 2, 3, 4, 5]) == 4
+    with raises(Exception):
+        data_range(1)
