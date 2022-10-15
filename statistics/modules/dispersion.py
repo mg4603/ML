a234173e5a6f8039d1cebdcaa9eec23bb17b0bd4
Michael George <michael.g.george@gmail.com>
2022-10-15 11:34:35 +0530

    Add fn data_range to dispersion, statistics

--- statistics/modules/dispersion.py
+++ statistics/modules/dispersion.py
@@ -4,3 +4,9 @@ def is_iterable(x):
         return True
     except TypeError:
         return False
+
+def data_range(x):
+    if is_iterable(x):
+        return max(x) - min(x)
+    else:
+        raise Exception('Range can\'t be calculated on given input')
