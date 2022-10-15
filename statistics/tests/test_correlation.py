694c103bae2e52c6a6ed947a6ee208493d0b83ba
Michael George <michael.g.george@gmail.com>
2022-10-15 15:41:38 +0530

    Test covariance in correlation, statistics

--- statistics/tests/test_correlation.py
+++ statistics/tests/test_correlation.py
@@ -8,3 +8,13 @@ def test_dot():
         dot([1, 2], 1)
     
     assert dot([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]) == 55
+
+def test_covariance():
+    with raises(Exception):
+        covariance(1, 2)
+    with raises(Exception):
+        covariance(1, [1, 2])
+    with raises(Exception):
+        covariance([1, 2], [3, 4, 5])
+    
+    assert covariance([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]) == 2.5
