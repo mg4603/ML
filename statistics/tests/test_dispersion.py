78e94030717a1d2a38ef9a2256bcacd7f6842c45
Michael George <michael.g.george@gmail.com>
2022-10-15 11:41:54 +0530

    Test de_mean in test_dispersion, statistics

--- statistics/tests/test_dispersion.py
+++ statistics/tests/test_dispersion.py
@@ -11,3 +11,9 @@ def test_data_range():
     assert data_range([1, 2, 3, 4, 5]) == 4
     with raises(Exception):
         data_range(1)
+
+def test_de_mean():
+    with raises(Exception):
+        de_mean(1)
+    
+    assert de_mean([1, 2, 3, 4, 5]) == [-2, -1, 0, 1, 2]
