f110a37f3d63c633479d4e66a59edb2a72804ba6
Michael George <michael.g.george@gmail.com>
2022-10-15 15:41:10 +0530

    Add fn covariance to correlation, statistics

--- statistics/modules/correlation.py
+++ statistics/modules/correlation.py
@@ -9,3 +9,12 @@ def dot(x, y):
         if n != len(y):
             raise Exception('Input vectors must be of the same size')
         return sum([i * j for i, j in zip(x, y)])
+
+def covariance(x, y):
+    if not is_collection(x) and not is_collection(y):
+        raise Exception("Input doesn't allow calculation of covariance")
+    else:
+        n = len (x)
+        if len(y) != n:
+            raise Exception('Input vectors must be of the same size')
+        return dot(de_mean(x), de_mean(y)) / (n-1)
